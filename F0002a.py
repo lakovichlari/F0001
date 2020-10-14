8c948a8512a04aa652f7bf6f564ec8066f38a9b2
lakovichlari <71382598+lakovichlari@users.noreply.github.com>
2020-10-14 14:05:00 +0200

    Add files via upload

---
+++ F0002a.py
@@ -0,0 +1,9 @@
+#F0002a: Melyik állat nevét nem írja ki a program? (Csak a válasz megadását követően ér lefuttatni a programot:))
+
+cica = 'cica'
+kutya = 'kutya'
+egér = 'egér'
+
+print('kutya')
+print('egér')
+print('cica')
